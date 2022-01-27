ba0e503821d411ea2de7831c1f19b1cb671e41fc
MishkaRogachev <mishkarogachev@gmail.com>
2022-01-27 11:24:47 +0300

    Add some more cesium stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { Viewer } from 'cesium';
+	import { Viewer, createWorldTerrain } from 'cesium';
 	import '../node_modules/cesium/Build/Cesium/Widgets/widgets.css'
 
 	window.CESIUM_BASE_URL = './build';
 
     let viewer: Viewer;
 	onMount(async () => {
-		viewer = new Viewer('cesiumContainer');
+		viewer = new Viewer('cesiumContainer', {
+            orderIndependentTranslucency: false,
+            timeline: false,
+            geocoder: false,
+            selectionIndicator: false,
+            infoBox: false,
+            scene3DOnly: true,
+            shouldAnimate: true,
+			terrainProvider: createWorldTerrain({
+                requestVertexNormals: true,
+                requestWaterMask: true
+            })
+		});
 	});
 
 </script>
